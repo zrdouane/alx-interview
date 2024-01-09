cc078a9dea93567cd9a8493cb9b30ee4aefc0e53
zrdouane <ramirdouane@gmail.com>
2024-01-09 10:59:42 +0100

    add readme

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-''' lockbox module '''
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
     '''
-        CanUnockAll
-        ([boxes]): a list of list
-    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True
-    for index, box in enumerate(boxes):
-        if unlocked[index]:
-            for index, key in enumerate(box):
-                if key < len(unlocked):
-                    unlocked[key] = True
-                    for i in boxes[key]:
-                        unlocked[i] = True
-    return all(unlocked)
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
